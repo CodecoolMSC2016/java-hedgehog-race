4432b4f7fd3b27e7b5c450dc34d29651317024c1
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2017-01-26 18:01:03 +0100

    Result.java bestBet done

--- Result.java
+++ Result.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Result
 {
 	private String name;
@@ -15,5 +17,34 @@ public class Result
 	public void setName(String name)
 	{
 		this.name = name;
-	}	
+	}
+	
+	public static List<String> bestBet() throws Exception
+	{
+		List<String> result = new ArrayList<String>();
+		HashMap<String, Integer> horsemap = new HashMap<String, Integer>();
+		horsemap = Horse.horseFrequency();
+		Integer simsum = 0;
+		result.add("");
+		result.add("0");
+		for (Integer i:horsemap.values())
+		{
+			if (Integer.parseInt(result.get(1)) < i)
+			{
+				result.set(1,Integer.toString(i));
+			}
+			simsum += i;
+		}
+		int maxwon = Integer.parseInt(result.get(1));
+		Integer onepercent = simsum/100;
+		result.set(1,Integer.toString(maxwon/onepercent));
+		for (Map.Entry<String, Integer> entry : horsemap.entrySet())
+		{
+            if (entry.getValue().equals(maxwon)) 
+			{
+                result.set(0,entry.getKey());
+			}
+		}
+		return result;
+	}
 }
