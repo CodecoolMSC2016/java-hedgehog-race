9a7125619145d6bf5199f891173f8c9f3e38ceb2
Trixi <bozso.beatrix@gmail.com>
2017-01-26 15:23:39 +0100

    merge problem solved

--- Main.java
+++ Main.java
@@ -3,21 +3,19 @@ import java.util.*;
 public class Main
 {	
 	public static List<Simulation> sims = new ArrayList<Simulation>();
+	
 	public static void main(String[] args)throws Exception
 	{
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		map = Horse.horseFrequency();
-		for(String i: map.keySet())
-		{
-			System.out.println(i + map.get(i));
-		}	
-	
+				
+		Logger.log("welcome");
+		Logger.log("money");
+		Logger.log("horse");
 	}
 	
 
 	public static void generateSimulation(int round) throws Exception
 		{
-		//  Not returns a Simulation instance that contains the simulation result
+//  Not returns a Simulation instance that contains the simulation result
 		for(int i = 1; i<= round; i++)
 		{
 			Simulation sim = new Simulation();
