1dbefc90015ee7ab903743c5da7c3cdc946f4a6b
tferenc <tamasferenc90@gmail.com>
2017-01-26 13:44:58 +0100

    main method tested

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
 import java.util.*;
 
 public class Main
-{	
+{	static int round;
 	public static List<Simulation> sims = new ArrayList<Simulation>();
 	public static void main(String[] args)throws Exception
-	{
+	{	
+		Scanner inp = new Scanner(System.in);
+		int inputNum = Integer.parseInt(inp.nextLine());
+		round = inputNum;
+		generateSimulation(round);
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		map = Horse.horseFrequency();
 		for(String i: map.keySet())
 		{
-			System.out.println(i + map.get(i));
+			System.out.println(i +" "+ map.get(i));
 		}	
 	
 	}
